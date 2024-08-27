4407404dd1a0a9504468b3accf65d96781768552
rhahao.vj <26148770+rhahao@users.noreply.github.com>
2024-08-27 10:01:13 +0000

    fix(api): route not rendered

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,6 @@ import { RouterBuilder } from 'next-api-handler';
 
 const router = new RouterBuilder();
 
-router.get((req, res) => res.status(200).json({ message: 'Crowdin Notifications API' }));
+router.get((req, res) => res.status(200).json({ message: 'Welcome to Crowdin Notifications API' }));
 
 export default router.build();
