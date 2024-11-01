f75ad428b20a6b856519f45c313cbf1cd41c6a99
rhahao <26148770+rhahao@users.noreply.github.com>
2024-11-01 11:56:42 +0300

    feat(api): include notification id in response

--- api/services/api.ts
+++ api/services/api.ts
@@ -3,6 +3,7 @@ import { AnnouncementItem } from '../definition/index.js';
 export const cleanUpFinalResponse = (announcements: AnnouncementItem[]) => {
   return announcements.map((announcement) => {
     return {
+      id: announcement.title.id,
       updatedAt: announcement.updatedAt,
       title: announcement.title.text,
       body: announcement.body.text,
